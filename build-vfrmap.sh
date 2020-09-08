fa56b90413d7a4f4cb30aec13b701c2ea492b4c0
Marph <github@perfectfamily.de>
2020-09-08 17:22:46 +0200

    more variables

--- build-vfrmap.sh
+++ build-vfrmap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 [ -x "$(command -v go-bindata)" ] && go generate github.com/MarphXL/msfs2020-go/simconnect
 [ -x "$(command -v go-bindata)" ] && go generate github.com/MarphXL/msfs2020-go/vfrmap
 [ -x "$(command -v go-bindata)" ] && go generate github.com/MarphXL/msfs2020-go/vfrmap/html/leafletjs
@@ -8,6 +7,6 @@ set -e
 build_time=$(date -u +'%Y-%m-%d_%T')
 set +e
 build_version=$(git describe --tags)
-set -e
+set -e 
 
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -X main.buildVersion=$build_version -X main.buildTime=$build_time" -v github.com/MarphXL/msfs2020-go/vfrmap
--- vfrmap/main.go
+++ vfrmap/main.go
@@ -28,21 +28,83 @@ import (
 // Marph was here
 type Report struct {
 	simconnect.RecvSimobjectDataByType
-	Title         [256]byte `name:"TITLE"`
-	Altitude      float64   `name:"INDICATED ALTITUDE" unit:"feet"` // PLANE ALTITUDE or PLANE ALT ABOVE GROUND
-	Latitude      float64   `name:"PLANE LATITUDE" unit:"degrees"`
-	Longitude     float64   `name:"PLANE LONGITUDE" unit:"degrees"`
-	Heading       float64   `name:"PLANE HEADING DEGREES TRUE" unit:"degrees"`
-	Airspeed      float64   `name:"AIRSPEED INDICATED" unit:"knot"`
-	AirspeedTrue  float64   `name:"AIRSPEED TRUE" unit:"knot"`
-	VerticalSpeed float64   `name:"VERTICAL SPEED" unit:"ft/min"`
-	Flaps         float64   `name:"TRAILING EDGE FLAPS LEFT ANGLE" unit:"degrees"`
-	Trim          float64   `name:"ELEVATOR TRIM PCT" unit:"percent"`
-	RudderTrim    float64   `name:"RUDDER TRIM PCT" unit:"percent"`
-	// neu
-	FuelQuantity          float64   `name:"FUEL TOTAL QUANTITY" unit:"gallons"`
-	FuelCapacity          float64   `name:"FUEL TOTAL CAPACITY" unit:"gallons"`
-	MasterIgnitionSwitch          float64   `name:"MASTER IGNITION SWITCH" unit:"on/off"`
+		AirspeedIndicated                       float64    `name:"AIRSPEED INDICATED" unit:"knots"` // Flight Instrumentation:Indicated airspeed
+		AirspeedTrue                            float64    `name:"AIRSPEED TRUE" unit:"knots"` // Flight Instrumentation:True airspeed
+		AiTrafficAssignedRunway                 [256]byte  `name:"AI TRAFFIC ASSIGNED RUNWAY" unit:"string"` // AI Controlled Aircraft:Assigned runway name (for example: "32R"). See 
+		AiTrafficState                          [256]byte  `name:"AI TRAFFIC STATE" unit:"string"` // AI Controlled Aircraft:English string describing an AI object's state.
+		AiTrafficToairport                      [256]byte  `name:"AI TRAFFIC TOAIRPORT" unit:"string"` // AI Controlled Aircraft:ICAO code of the destination airport in the cur
+		AmbientTemperature                      float64    `name:"AMBIENT TEMPERATURE" unit:"celsius"` // Environment:Ambient temperature
+		AtcFlightNumber                         [256]byte  `name:"ATC FLIGHT NUMBER" unit:"string (6)"` // String:Flight Number used by ATC
+		AtcId                                   [256]byte  `name:"ATC ID" unit:"string (10)"` // String:ID used by ATC
+		AtcModel                                [256]byte  `name:"ATC MODEL" unit:"string (10)"` // String:Model used by ATC
+		AtcType                                 [256]byte  `name:"ATC TYPE" unit:"string (30)"` // String:Type used by ATC
+		AutopilotAvailable                      bool       `name:"AUTOPILOT AVAILABLE" unit:"bool"` // Autopilot:Available flag
+		AutopilotFlightDirectorActive           bool       `name:"AUTOPILOT FLIGHT DIRECTOR ACTIVE" unit:"bool"` // Autopilot:Flight director active
+		AutopilotMaster                         bool       `name:"AUTOPILOT MASTER" unit:"bool"` // Autopilot:On/off flag
+		AvatarModeIsAttached                    bool       `name:"AVATAR MODE IS ATTACHED" unit:"bool"` // Avatar Mode:Sets/gets the attachment state of the user avatar.
+		AvionicsMasterSwitch                    bool       `name:"AVIONICS MASTER SWITCH" unit:"bool"` // Avionics:Avionics switch state
+		BarometerPressure                       float64    `name:"BAROMETER PRESSURE" unit:"millibars"` // Environment:Barometric pressure
+		BrakeIndicator                          float64    `name:"BRAKE INDICATOR" unit:"position"` // Avionics:Brake on indication [0.0: Off, 1.0: Full Brakes]
+		BrakeParkingIndicator                   bool       `name:"BRAKE PARKING INDICATOR" unit:"bool"` // Avionics:Parking brake indicator
+		BrakeParkingPosition                    bool       `name:"BRAKE PARKING POSITION" unit:"bool"` // Avionics:Parking brake on [0: Off, 1.0: Full Parking Brake]
+		ElectricalBatteryLoad                   float64    `name:"ELECTRICAL BATTERY LOAD" unit:"ampere"` // Miscellaneous Systems:Battery load
+		ElectricalMainBusVoltage                float64    `name:"ELECTRICAL MAIN BUS VOLTAGE" unit:"volts"` // Miscellaneous Systems:Main bus voltage
+		ElectricalMasterBattery                 bool       `name:"ELECTRICAL MASTER BATTERY" unit:"bool"` // Miscellaneous Systems:Battery switch position
+		ElevatorDeflectionPct                   float64    `name:"ELEVATOR DEFLECTION PCT" unit:"percent over 100"` // Avionics:Percent deflection
+		ElevatorTrimPct                         float64    `name:"ELEVATOR TRIM PCT" unit:"percent over 100"` // Avionics:Percent elevator trim
+		EmptyWeight                             float64    `name:"EMPTY WEIGHT" unit:"pounds"` // Miscellaneous:Empty weight of the aircraft
+		EngMaxRpm                               float64    `name:"ENG MAX RPM" unit:"rpm"` // Engine:Maximum rpm
+		FlapsAvailable                          bool       `name:"FLAPS AVAILABLE" unit:"bool"` // Avionics:True if flaps available
+		FlapsHandleIndex                        float64    `name:"FLAPS HANDLE INDEX" unit:"number"` // Avionics:Index of current flap position
+		FlapsHandlePercent                      float64    `name:"FLAPS HANDLE PERCENT" unit:"percent over 100"` // Avionics:Percent flap handle extended
+		FlapsNumHandlePositions                 float64    `name:"FLAPS NUM HANDLE POSITIONS" unit:"number"` // Avionics:Number of flap positions
+		FuelTotalCapacity                       float64    `name:"FUEL TOTAL CAPACITY" unit:"gallons"` // Fuel:Total capacity of the aircraft
+		FuelTotalQuantity                       float64    `name:"FUEL TOTAL QUANTITY" unit:"gallons"` // Fuel:Current quantity in volume
+		FuelTotalQuantityWeight                 float64    `name:"FUEL TOTAL QUANTITY WEIGHT" unit:"pounds"` // Fuel:Current total fuel weight of the aircraft
+		GearTotalPctExtended                    float64    `name:"GEAR TOTAL PCT EXTENDED" unit:"percent"` // Landing Gear:Percent total gear extended
+		GeneralEngStarterActive                 bool       `name:"GENERAL ENG STARTER ACTIVE" unit:"bool"` // Engine:True if engine starter is active
+		GpsApproachAirportId                    [256]byte  `name:"GPS APPROACH AIRPORT ID" unit:"string"` // String:ID of airport
+		GpsGroundSpeed                          float64    `name:"GPS GROUND SPEED" unit:"meters per second"` // Avionics:Current ground speed
+		HeadingIndicator                        float64    `name:"HEADING INDICATOR" unit:"radians"` // Flight Instrumentation:Heading indicator (directional gyro) indication
+		IndicatedAltitude                       float64    `name:"INDICATED ALTITUDE" unit:"feet"` // Flight Instrumentation:Altimeter indication
+		LightBeaconOn                           bool       `name:"LIGHT BEACON ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightBrakeOn                            bool       `name:"LIGHT BRAKE ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightCabinOn                            bool       `name:"LIGHT CABIN ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightHeadOn                             bool       `name:"LIGHT HEAD ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightLandingOn                          bool       `name:"LIGHT LANDING ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightLogoOn                             bool       `name:"LIGHT LOGO ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightNavOn                              bool       `name:"LIGHT NAV ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightPanelOn                            bool       `name:"LIGHT PANEL ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightRecognitionOn                      bool       `name:"LIGHT RECOGNITION ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightStrobeOn                           bool       `name:"LIGHT STROBE ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightTaxiOn                             bool       `name:"LIGHT TAXI ON" unit:"bool"` // Lights:Return true if the light is on.
+		LightWingOn                             bool       `name:"LIGHT WING ON" unit:"bool"` // Lights:Return true if the light is on.
+		MagneticCompass                         float64    `name:"MAGNETIC COMPASS" unit:"degrees"` // Miscellaneous:Compass reading
+		MasterIgnitionSwitch                    bool       `name:"MASTER IGNITION SWITCH" unit:"bool"` // Engine:Aircraft master ignition switch (grounds all engines magnetos)
+		MaxGrossWeight                          float64    `name:"MAX GROSS WEIGHT" unit:"pounds"` // Miscellaneous:Maximum gross weight of the aircraft
+		NumberOfEngines                         float64    `name:"NUMBER OF ENGINES" unit:"number"` // Engine:Number of engines (minimum 0, maximum 4)
+		OverspeedWarning                        bool       `name:"OVERSPEED WARNING" unit:"bool"` // Flight Instrumentation:Overspeed warning state
+		PlaneAltAboveGround                     float64    `name:"PLANE ALT ABOVE GROUND" unit:"feet"` // Position and Speed:Altitude above the surface
+		PlaneHeadingDegreesTrue                 float64    `name:"PLANE HEADING DEGREES TRUE" unit:"radians"` // Position and Speed:Heading relative to true north, although the name m
+		PlaneLatitude                           float64    `name:"PLANE LATITUDE" unit:"radians"` // Position and Speed:Latitude of aircraft, North is positive, South nega
+		PlaneLongitude                          float64    `name:"PLANE LONGITUDE" unit:"radians"` // Position and Speed:Longitude of aircraft, East is positive, West negat
+		RudderPosition                          float64    `name:"RUDDER POSITION" unit:"position"` // Avionics:Rudder input deflection [-1.0: Full Left, 1.0: Full Right]
+		RudderTrim                              float64    `name:"RUDDER TRIM" unit:"radians"` // Avionics:Angle deflection
+		RudderTrimPct                           float64    `name:"RUDDER TRIM PCT" unit:"percent over 100"` // Avionics:Percent deflection
+		RudderTrimPct                           float64    `name:"RUDDER TRIM PCT" unit:"percent over 100"` // Avionics:The trim position of the rudder. Zero is no trim.
+		SimOnGround                             bool       `name:"SIM ON GROUND" unit:"bool"` // Position and Speed:On ground flag (To set, passing a non-zero argument
+		SpoilersHandlePosition                  float64    `name:"SPOILERS HANDLE POSITION" unit:"position"` // Avionics:Spoiler handle position [0: Retracted, 1.0: Fully Extended]
+		StallWarning                            bool       `name:"STALL WARNING" unit:"bool"` // Flight Instrumentation:Stall warning state
+		SurfaceCondition                        float64    `name:""SURFACE CONDITION"" unit:""enum""` // Miscellaneous:One of:0: Normal 1: Wet 2: Icy 3: Snow"
+		Title                                   [256]byte  `name:"TITLE" unit:"variable length string"` // String:title from aircraft.cfg
+		TotalWeight                             float64    `name:"TOTAL WEIGHT" unit:"pounds"` // Miscellaneous:Total weight of the aircraft
+		TrailingEdgeFlapsLeftAngle              float64    `name:"TRAILING EDGE FLAPS LEFT ANGLE" unit:"radians"` // Avionics:Angle left trailing edge flap extended. Use TRAILING EDGE FLA
+		TrailingEdgeFlapsRightAngle             float64    `name:"TRAILING EDGE FLAPS RIGHT ANGLE" unit:"radians"` // Avionics:Angle right trailing edge flap extended. Use TRAILING EDGE FL
+		TransponderCode_i                       float64    `name:"TRANSPONDER CODE:index" unit:"bco16"` // Avionics:4-digit code
+		UnlimitedFuel                           bool       `name:"UNLIMITED FUEL" unit:"bool"` // Fuel:Unlimited fuel flag
+		VariometerRate                          float64    `name:"VARIOMETER RATE" unit:"feet per second"` // Miscellaneous:Variometer rate
+		VerticalSpeed                           float64    `name:"VERTICAL SPEED" unit:"feet per second"` // Flight Instrumentation:Vertical speed indication
+
 }
 
 func (r *Report) RequestData(s *simconnect.SimConnect) {
